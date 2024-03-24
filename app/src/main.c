c9ec87ad4b6c93166ecdcde4dbe2e0b5587da902
khorie <kentah10@gmail.com>
2024-03-24 01:25:26 -0700

    new lib

--- app/src/main.c
+++ app/src/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "foo.h"
 #include "hal/distanceSensor.h"
 #include "hal/shared.h"
 int main() {
-  DS_init();
-  while (1) {
+  bool sensor = false;
+  
+  if(sensor){
+    DS_init();
+    while (1) {
     printf("Hello World\n");
     printf("Gyro Test");
     printf("Distance = %d\n", DS_getReading());
     sleepForMs(1000);
   }
+  }
+
   return 0;
 }
