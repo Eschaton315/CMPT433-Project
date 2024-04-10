b569ac24fa8daccfb71b21acaa7e78036c8dce14
khorie <kentah10@gmail.com>
2024-04-09 22:54:48 -0700

    Added comments for main loop

--- app/src/main.c
+++ app/src/main.c
@@ -15,7 +15,6 @@
 //Change value depending on whether the hardware is installed or not.
 #define DISTANCE_SENSOR false
 #define GYROSCOPE true
-//#include "hal/getbno080.h"
 #define I2C_ADDR 0x68
 
 
@@ -26,6 +25,8 @@ int main() {
   bool fall  = false;
   long long fallTimer = 0;
   long long currentTime = 0;
+
+  //initialize hardware
 
   if(DISTANCE_SENSOR){
     DS_init();
@@ -44,10 +45,10 @@ int main() {
     yaw = gyroData[0];
     roll = gyroData[1];
     pitch = gyroData[2];
-    
+    //prints gyro value per 0.1 sec.
     printf("Yaw: %0.2f Roll: %0.2f  Pitch: %0.2f \n",yaw,roll,pitch);
     if(yaw>70||yaw<-70||pitch>70||pitch<-70){
-
+      //if yaw or pitch is over a set value, wait if it stays in that range to detect a fall.
       if(!fall){
         fall = true;
         fallTimer = getTimeInMs();
@@ -55,6 +56,7 @@ int main() {
         currentTime = getTimeInMs();
         if(currentTime-fallTimer>1500){
           //if the stick is parallel to the ground for over 1.5 seconds the stick will recognize a fall
+          //The code willl print FALLEN until left joystick is pressed
           while(joystick_getJoystickValue()!=3){
             printf("FALLEN\n");
           }
@@ -64,6 +66,7 @@ int main() {
     }else{
       fall = false;
     }
+    //attempt  at resetting the gyro value. Doesn't work as intended for now. 
     /*
     if(joystick_getJoystickValue() == 5){
       //recalibrate joystick
