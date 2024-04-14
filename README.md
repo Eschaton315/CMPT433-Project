fcedb835af4fabd84176e85621bce099e306781e
Ian Hwang <ian807b@gmail.com>
2024-04-13 21:37:23 -0700

    Added comments for motor/DS

--- README.md
+++ README.md
@@ -1,35 +1,53 @@
 # CMPT 433 Smart Stick Structure
 
 ```
-CMPT433-Project
-├─ CMakeLists.txt
-├─ README.md
-├─ app
-│  ├─ CMakeLists.txt
-│  ├─ include
-│  │  └─ foo.h
-│  └─ src
-│     ├─ foo.c
-│     └─ main.c
-├─ hal
-│  ├─ CMakeLists.txt
-│  ├─ include
-│  │  └─ hal
-│  │     └─ distanceSensor.h
-│  │     └─ shared.h
-│  └─ src
-│     └─ distanceSensor.c
-│     └─ shared.c
-└─ lib
-   └─ VL53L0X
-      ├─ LICENSE.txt
-      ├─ Makefile
-      ├─ README.md
-      ├─ libtof.a
-      ├─ main.c
-      ├─ make_demo
-      ├─ tof.c
-      ├─ tof.h
-      └─ tof.o
+.
+├── app
+│   ├── CMakeLists.txt
+│   ├── include
+│   │   ├── foo.h
+│   │   ├── organize.h
+│   │   └── pwmDriver.h
+│   └── src
+│       ├── foo.c
+│       ├── main.c
+│       ├── organize.c
+│       └── pwmDriver.c
+├── CMakeLists.txt
+├── hal
+│   ├── CMakeLists.txt
+│   ├── include
+│   │   └── hal
+│   │       ├── distanceSensor.h
+│   │       ├── gyroscope.h
+│   │       ├── I2Cdev.h
+│   │       ├── joyStick.h
+│   │       ├── motor.h
+│   │       ├── MPU6050.h
+│   │       ├── pwm.h
+│   │       └── shared.h
+│   └── src
+│       ├── bno085Scrap
+│       ├── distanceSensor.c
+│       ├── gyroscope.c
+│       ├── I2Cdev.c
+│       ├── joyStick.c
+│       ├── motor.c
+│       ├── MPU6050.c
+│       ├── pwm.c
+│       └── shared.c
+├── lib
+│   └── VL53L0X
+│       ├── libtof.a
+│       ├── LICENSE.txt
+│       ├── main.c
+│       ├── make_demo
+│       ├── Makefile
+│       ├── README.md
+│       ├── tof.c
+│       ├── tof.h
+│       └── tof.o
+└── README.md
+
 
 ```
--- hal/src/distanceSensor.c
+++ hal/src/distanceSensor.c
@@ -1,9 +1,10 @@
 #include "hal/distanceSensor.h"
-#include "hal/shared.h"
 
-#include <tof.h>
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <tof.h>
+
+#include "hal/shared.h"
 
 volatile bool DS_DRIVER_FLAG = true;
 static pthread_t sensorThreadID;
@@ -17,12 +18,12 @@ void *sensor_Thread(void *);
 void DS_init(void) {
   runCommand("config-pin p9.17 i2c");
   runCommand("config-pin p9.18 i2c");
-  tofInit(1, DISTANCE_SENSOR_ADDR, 0);// Call tof library init function
+  tofInit(1, DISTANCE_SENSOR_ADDR, 0);  // Call tof library init function
   printf("DS_init called\n");
   pthread_create(&sensorThreadID, NULL, sensor_Thread, NULL);
 }
 
-void DS_cleanup(void){
+void DS_cleanup(void) {
   printf("DS_cleanup called\n");
   DS_DRIVER_FLAG = false;
   pthread_join(sensorThreadID, NULL);
@@ -32,21 +33,18 @@ void DS_cleanup(void){
 // Wrapper function to call tofReadDistance() from tof library
 static int DS_getReading() { return tofReadDistance(); }
 
-
 /*
 This thread periodically updates the distance variable every 50 milliseconds
 Thread stops when the Distance Sensor Driver flag is set to false
 */
-void *sensor_Thread(void *arg){
-  (void) arg;
-  srand(time(0)); 
-  while(DS_DRIVER_FLAG){
-	  distance = (float) DS_getReading() / 10;
+void *sensor_Thread(void *arg) {
+  (void)arg;
+  while (DS_DRIVER_FLAG) {
+    distance = (float)DS_getReading() / 10;
     sleepForMs(50);
   }
   return NULL;
 }
 
-float distance_getData(){
-	return distance;
-}
+// Getter function for the distance reading
+float distance_getData() { return distance; }
